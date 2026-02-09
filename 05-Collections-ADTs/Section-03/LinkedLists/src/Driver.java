65e30e7b4d3e925b028717aa44548f80efbe0561
ReeseHatfield <reesehatfield02@gmail.com>
2026-02-09 13:17:01 -0500

    linked lists

---
+++ 05-Collections-ADTs/Section-03/LinkedLists/src/Driver.java
@@ -0,0 +1,69 @@
+import java.util.*;
+
+public class Driver {
+    public static void main(String[] args){
+        int[] arr = new int[5];
+        int[] arr1 = {1, 2  };
+
+
+
+        List<String> l1 = new LinkedList<>();
+
+        l1.add("hello");
+        l1.add("hello");
+        l1.add("hello");
+        l1.add("hello");
+        l1.add("hello");
+        l1.add("hello");
+        l1.add("hello");
+        l1.add("hello");
+        l1.add("hello");
+        l1.add("hello");
+
+        l1.get(0);
+
+
+        Node<String> n0 = new Node<>("Alice");
+        
+        Node<String> n1 = new Node<>("Bob");
+        n0.next = n1;
+        
+        Node<String> n2 = new Node<>("Charlie");
+        n1.next = n2;
+
+        n2.next = null;
+
+
+        // System.out.println(n0.next.next.data);
+
+        for(int i = 0; i < 10; i ++){
+
+        }
+
+
+        Node<String> current = n0;
+        while(current != null){
+
+            System.out.println(current.data);
+
+            current = current.next;
+        }
+
+
+
+        // initialize; conditional; modification
+        for(int i = 0; i < 10; i ++){
+
+        }
+
+        System.out.println();
+
+        for(Node<String> cur = n0; cur != null; cur = cur.next){
+            System.out.println(cur.data);
+        }
+
+
+        // Node<Integer> n3 = new Node<>(5);
+
+    }
+}

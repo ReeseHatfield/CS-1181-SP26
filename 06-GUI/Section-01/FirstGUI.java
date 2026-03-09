ac178f5e0f593784e17afd62d1566f33d4b8260f
Clarissa Milligan <milligan.s.clarissa@gmail.com>
2026-03-09 14:33:03 -0400

    GUI intro

---
+++ 06-GUI/Section-01/FirstGUI.java
@@ -0,0 +1,17 @@
+import javax.swing.*;
+
+public class FirstGUI
+{
+    public static void main(String[] args)
+    {
+        JFrame mainWindow = new JFrame("My first GUI!!!");
+        mainWindow.setVisible(true);
+        mainWindow.setSize(300, 500);
+        mainWindow.setLocation(500, 300);
+        mainWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        while (true)
+        {
+            System.out.println("All done!!");
+        }
+    }
+}
---
+++ 06-GUI/Section-01/MyFrame.java
@@ -0,0 +1,6 @@
+import javax.swing.*;
+
+public class MyFrame extends JFrame
+{
+    private int myFavNum = 3;
+}

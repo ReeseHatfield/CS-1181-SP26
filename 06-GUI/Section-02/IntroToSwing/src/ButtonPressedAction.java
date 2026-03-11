9a5abe888ee08578389f4332929fa2a763729f84
ReeseHatfield <reesehatfield02@gmail.com>
2026-03-11 10:12:25 -0400

    mar 11

---
+++ 06-GUI/Section-02/IntroToSwing/src/ButtonPressedAction.java
@@ -0,0 +1,11 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class ButtonPressedAction implements ActionListener {
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        System.out.println("Button was pressed");
+    }
+    
+}
--- 06-GUI/Section-02/IntroToSwing/src/Driver.java
+++ 06-GUI/Section-02/IntroToSwing/src/Driver.java
@@ -1,5 +1,6 @@
 import javax.swing.*;
 import java.awt.*;
+import java.util.Random;
 
 public class Driver {
 
@@ -7,14 +8,22 @@ public class Driver {
     // Declare component
     // Set up the component
     // Render component
+
     public static void main(String[] args){
+
+        FontAdjuster.adjustFontSize(20);
+
+
+
         JFrame frame = new JFrame();
 
         JPanel root = new JPanel();
 
 
-        JTextArea input = new JTextArea();
+        JTextArea input = new JTextArea(5, 5);
+        
         root.add(input);
+
 
         JLabel helloLabel = new JLabel();
         helloLabel.setText("Hello world");
@@ -23,6 +32,45 @@ public class Driver {
 
         JLabel welcomeLabel = new JLabel("Welcome to my first GUI app");
         root.add(welcomeLabel);
+
+
+        JButton btn = new JButton("My First JButton");
+        // btn.addActionListener(new ButtonPressedAction());
+        btn.addActionListener((e) -> {
+
+            
+            System.out.println("I was called from a lambda");
+            String userInput = input.getText();
+
+            JLabel l = new JLabel("You said: " + userInput);
+            root.add(l);
+
+            // root.repaint();
+            root.revalidate();
+
+            // closes the jframe
+            // frame.dispose();
+
+
+            // spawns new jframes
+
+            // increase this number to crash your system
+            for(int i = 0; i < 10; i ++){
+                
+                JFrame f = new JFrame();
+                Random rng = new Random();
+    
+                f.setLocation(rng.nextInt(0, 1000), rng.nextInt(0, 1000));
+                f.setSize(250,250);
+                f.setVisible(true);
+            }
+
+
+        });
+        root.add(btn);
+
+
+
 
 
         frame.setContentPane(root);

a2fcc3a18fba4ddeed7a440e9522ebbdaa7d21a3
ReeseHatfield <reesehatfield02@gmail.com>
2026-03-11 13:29:18 -0400

    more swing

--- 06-GUI/Section-03/IntroToSwing/src/Driver.java
+++ 06-GUI/Section-03/IntroToSwing/src/Driver.java
@@ -1,14 +1,25 @@
 import javax.swing.*;
 import java.awt.*;
+import java.util.Random;
 
 public class Driver {
     // Declaration
     // Setup initialization of the component
     // Rendering of the component
+
     public static void main(String[] args) {
+
+
+        FontAdjuster.adjustFontSize(20);
+
         JFrame frame = new JFrame();
 
         JPanel root = new JPanel();
+
+
+        JTextArea input = new JTextArea(5, 5);
+        root.add(input);
+
 
         JLabel helloLabel = new JLabel();
         helloLabel.setText("Hello world!");
@@ -18,14 +29,53 @@ public class Driver {
         welcomeLabel.setText("Welcome to my first GUI");
         root.add(welcomeLabel);
 
-        JTextArea input = new JTextArea(5, 100);
-        root.add(input);
+        JButton btn = new JButton("My first btn");
+        // btn.addActionListener(new ButtonPressedAction());
 
+        // (e) -> {
+
+        // }
+
+        root.add(btn);
+        
+        JColorChooser chooser = new JColorChooser();
+        root.add(chooser);
+        btn.addActionListener((e) -> {
+            System.out.println("Hello! I was pressed");
+
+            root.add(new JLabel("Button was pressed!"));
+
+            root.revalidate();
+
+            // frame.pack();
+
+            Random rng = new Random();
+            for(int i = 0; i < 10; i ++){
+                JFrame f = new JFrame();
+
+                Color chosenColor = chooser.getColor();
+
+                JButton temp = new JButton();
+                temp.addActionListener((e2) -> {
+                    System.out.println(chosenColor);
+                });
+                temp.setBackground(chosenColor);
+
+                f.add(temp);
+
+                f.setSize(250,250);
+                f.setLocation(rng.nextInt(0,1000), rng.nextInt(0, 1000));
+                f.setVisible(true);
+
+
+            }
+            // root.repaint();
+        });
 
         frame.setContentPane(root);
         frame.setSize(500, 500);
         frame.setTitle("My first GUI");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        // frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         frame.setVisible(true);
     }

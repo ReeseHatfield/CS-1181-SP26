6728ad827d4110f26f37546f18555dbc117ec59a
Clarissa Milligan <milligan.s.clarissa@gmail.com>
2026-03-11 12:28:14 -0400

    buttons and panels

--- 06-GUI/Section-01/FirstGUI.java
+++ 06-GUI/Section-01/FirstGUI.java
@@ -4,14 +4,14 @@ public class FirstGUI
 {
     public static void main(String[] args)
     {
-        JFrame mainWindow = new JFrame("My first GUI!!!");
+        JFrame mainWindow = new MyFrame("My first GUI!!!");
         mainWindow.setVisible(true);
         mainWindow.setSize(300, 500);
         mainWindow.setLocation(500, 300);
         mainWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        while (true)
-        {
-            System.out.println("All done!!");
-        }
+//        while (true)
+//        {
+//            System.out.println("All done!!");
+//        }
     }
 }
--- 06-GUI/Section-01/MyFrame.java
+++ 06-GUI/Section-01/MyFrame.java
@@ -1,6 +1,37 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class MyFrame extends JFrame
 {
-    private int myFavNum = 3;
+    private int clickCount = 0;
+    private JPanel contentPanel = new JPanel();
+
+    public MyFrame(String frameTitle)
+    {
+        super(frameTitle);
+
+        JButton clickMe = new JButton("Click me!");
+        clickMe.setSize(100, 100);
+        //clickMe.setPreferredSize(new Dimension(100, 100));
+        clickMe.addActionListener(e -> {
+            System.out.println("I have been clicked " + ++clickCount + " number of times!");
+        });
+        contentPanel.add(clickMe);
+
+        JButton newButton = new JButton("I am a new button!!!");
+        contentPanel.add(newButton);
+
+        addTenButtons();
+
+        this.add(contentPanel);
+    }
+
+    public void addTenButtons()
+    {
+        for (int i = 1; i <= 10; i++)
+        {
+            JButton button = new JButton("Button #" + i);
+            contentPanel.add(button);
+        }
+    }
 }

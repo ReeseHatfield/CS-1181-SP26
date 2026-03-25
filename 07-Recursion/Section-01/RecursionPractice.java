37ab993301309922e7d8dff5e399ce29856305f8
Clarissa Milligan <milligan.s.clarissa@gmail.com>
2026-03-25 12:14:35 -0400

    more recursive practice

--- 07-Recursion/Section-01/RecursionPractice.java
+++ 07-Recursion/Section-01/RecursionPractice.java
@@ -1,9 +1,19 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class RecursionPractice
 {
     public static void main(String[] args)
     {
         System.out.println(reverseWord(""));
         System.out.println(countEvens("1"));
+
+        ArrayList<String> locations = new ArrayList<>(Arrays.asList("school", "zoo", "museum", "mall", "bank"));
+        ArrayList<Integer> nums = new ArrayList<>(Arrays.asList(1, 5, -9, 6, 3, 28, -29, 5, 10));
+        ArrayList<Double> gpas = new ArrayList<>();
+        System.out.println(findMax(locations));
+        System.out.println(findMax(nums));
+        System.out.println(findMax(gpas));
     }
 
     public static String reverseWord(String word)
@@ -39,12 +49,32 @@ public class RecursionPractice
 //        return isEven(firstNum) + countEvens(restOfNum);
     }
 
-    public static int isEven(int num)
+    private static int isEven(int num)
     {
         if (num % 2 == 0)
         {
             return 1;
         }
         return 0;
+    }
+
+    public static <I extends Comparable<I>> I findMax(ArrayList<I> items)
+    {
+        if (items.isEmpty())
+        {
+            return null;
+        }
+        else if (items.size() == 1)
+        {
+            return items.getFirst();
+        }
+
+        I firstElement = items.removeFirst();
+        I maxElement = findMax(items);
+        if (firstElement.compareTo(maxElement) < 0)
+        {
+            return maxElement;
+        }
+        return firstElement;
     }
 }

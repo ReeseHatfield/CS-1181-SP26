0cbcb2592c5b0e90665683c804bec4ee3b1f0d96
ReeseHatfield <reesehatfield02@gmail.com>
2026-04-08 13:24:24 -0400

    more sorting

--- 08-Searching-Sorting-Algorithms/Section-03/Sorting/src/Driver.java
+++ 08-Searching-Sorting-Algorithms/Section-03/Sorting/src/Driver.java
@@ -6,16 +6,31 @@ import java.util.Random;
 public class Driver {
     public static void main(String[] args) {
 
-        List<Integer> nums = new ArrayList<>();
-        
-        fillWithRandom(nums);
 
-        System.out.println("Unsorted:");
-        System.out.println(nums);
-        System.out.println();
-        System.out.println("Sorted:");
-        nums = bubbleSort(nums);
-        System.out.println(nums);
+        int a = 5;
+        int b = 10;
+
+        // int temp = b; // 5
+
+        // b = a; 
+        // a = temp;
+
+
+        a = a + b; // 5 + 10 = 15
+        b = a - b; // 15 - 10 = 5
+        a = a - b; // 15 - 5 = 10
+
+
+        // List<Integer> nums = new ArrayList<>();
+        
+        // fillWithRandom(nums);
+
+        // System.out.println("Unsorted:");
+        // System.out.println(nums);
+        // System.out.println();
+        // System.out.println("Sorted:");
+        // nums = mergeSort(nums);
+        // System.out.println(nums);
 
 
     }
@@ -43,9 +58,24 @@ public class Driver {
 
         Random rng = new Random();
 
-        for(int i = 0; i < 100; i++) {
+        for(int i = 0; i < 1_000_000; i++) {
             list.add(rng.nextInt(0, 100));
         }
+    }
+
+    public static List<Integer> mergeSort(List<Integer> list){
+        if(list.size() <= 1){
+            return list;
+        }
+
+        int middle = list.size() / 2;
+        List<Integer> left = new ArrayList<>(list.subList(0, middle));
+        List<Integer> right = new ArrayList<>(list.subList(middle, list.size()));
+
+        left = mergeSort(left);
+        right = mergeSort(right);
+
+        return merge(left, right);
     }
 
 
